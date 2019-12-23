7a93253f3c97f7f6b37f221c78702fb2fa207da7
Edws <eduardos.rocha17@gmail.com>
2019-12-23 13:33:06 -0300

    Criando font.lua
    Facilita o uso de fontes

---
+++ font.lua
@@ -0,0 +1,6 @@
+local Fonts = {}
+
+Fonts.PressStart2P = love.graphics.newFont("font/PressStart2P-Regular/PressStart2P-Regular.ttf", 8)
+Fonts.PressStart2P:setFilter("nearest", "nearest")
+
+return Fonts
