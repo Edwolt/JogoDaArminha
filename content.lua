ae56627eba91c3463a44cc0dd7f1bfcd59be60b2
Edws <eduardos.rocha17@gmail.com>
2019-12-23 19:06:09 -0300

    Adição de Credits ao import de Content

--- content.lua
+++ content.lua
@@ -1,5 +1,6 @@
 local Content = Content or {}
 Content.Menu = Content.Menu or require "content.menu"
+Content.Credits = Content.Credits or require "content.credits"
 Content.Game = Content.Game or require "content.game"
 
 function Content:new()
