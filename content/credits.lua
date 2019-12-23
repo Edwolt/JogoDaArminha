7d24c1cdf1b8c5e6dcaeabd9eaba692c21fc4eea
Edws <eduardos.rocha17@gmail.com>
2019-12-23 19:58:13 -0300

    Modificando a Aparencia do Credito

--- content/credits.lua
+++ content/credits.lua
@@ -3,8 +3,20 @@ Content = Content or {}
 Content.Menu = Content.Menu or require "content.menu"
 local Menu = Content.Menu
 
+--* Line Table
+local function newLine(text, scale)
+    return {
+        text = text or "",
+        scale = scale or 3
+    }
+end
+
+--* Credits Class
 Credits = {
     lines = {
+        newLine("Autores:", 5),
+        newLine("Eduardo Souza Rocha"),
+        newLine("Victor Isaias de Almeida Rocha"),
     }
 }
 Credits.__index = Credits
@@ -20,7 +32,7 @@ function Credits:new()
         local y = 50
         local limit = (UTIL.window.width - 100)
         for _, i in ipairs(Credits.lines) do
-            y = y + UTIL.printw(i, Fonts.PressStart2P, 50, y, limit, "center", 3)
+            y = y + UTIL.printw(i.text, Fonts.PressStart2P, 50, y, limit, "center", i.scale)
         end
     end
 
