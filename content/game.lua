eeeb02cb9e56bc5bf36f3f613d70eae5e027df35
Edws <eduardos.rocha17@gmail.com>
2019-12-24 12:31:36 -0300

    Game tem um array de Bullet

--- content/game.lua
+++ content/game.lua
@@ -1,21 +1,30 @@
+Modules = Modules or require "modules"
+local Vec = Modules.Vec
+local Array = Modules.Array
+
 --* Game Class
 local Game = {
-    Player = require "game.player"
+    Player = require "game.player",
+    Bullet = require "game.bullet"
 }
+
 Game.__index = Game
 
 function Game:new()
     local game = {
-        player = Game.Player:new()
+        player = Game.Player:new(),
+        bullets = Array:new(Game.Bullet)
     }
     setmetatable(game, self)
 
     function game:draw()
         self.player:draw()
+        self.bullets:draw()
     end
 
     function game:update(dt)
         self.player:update(dt)
+        self.bullets:update(dt)
     end
 
     function game:escape()
