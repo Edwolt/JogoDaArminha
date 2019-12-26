6870afc7186f8277a5b78188257263e09183881e
Edws <eduardos.rocha17@gmail.com>
2019-12-26 17:57:32 -0300

    Bullet volta a cor depois se desenhar

--- game/bullet.lua
+++ game/bullet.lua
@@ -26,8 +26,9 @@ function Bullet:new(weapon, pos, vel)
         else
             love.graphics.setColor(255, 255, 255)
         end
-
         love.graphics.rectangle("fill", real_pos.x, real_pos.y, 2 * UTIL.game.scale, 2 * UTIL.game.scale)
+        
+        love.graphics.setColor(255, 255, 255)
     end
 
     function Bullet:update(dt)
