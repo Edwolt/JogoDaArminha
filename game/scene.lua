f849001abba9c7d243694ca6e7a2d534f959f0f4
Edws <eduardos.rocha17@gmail.com>
2019-12-26 13:50:29 -0300

    Em scene: sheet inserido na tabela tileset e função draw foi iniciada
    para guardar dados importantes, criou uma campo tileset em scene e sheet foi movido para ele
    A função daw foi iniciada

--- game/scene.lua
+++ game/scene.lua
@@ -1,5 +1,7 @@
+UTIL = UTIL or require "util"
 Modules = Modules or require "modules"
 local Colliders = Modules.Colliders
+local Vec = Modules.Vec
 
 --* Scene Class
 local Scene = {}
@@ -7,10 +9,20 @@ Scene.__index = Scene
 
 function Scene:new(path)
     local tilemap = require("tilemap/" .. path)
-    -- sheet
-    local tileset = tilemap.tilesets[1]
-    local sheet = love.graphics.newImage(tileset.image:sub(3))
-    sheet:setFilter("nearest", "nearest")
+
+    -- sheet and others informations
+    local _tileset = tilemap.tilesets[1]
+    local tileset = {
+        sheet = love.graphics.newImage(_tileset.image:sub(3)),
+        -- Tile dimensions
+        twidth = _tileset.tilewidth,
+        theight = _tileset.tileheight,
+        -- Image dimensions
+        iwidth = _tileset.imagewidth,
+        iheight = _tileset.imageheight,
+        columns = _tileset.columns
+    }
+    tileset.sheet:setFilter("nearest", "nearest")
 
     -- layer
     local layer = tilemap.layers[1]
@@ -25,12 +37,12 @@ function Scene:new(path)
                 -- Air: Do nothing
             elseif 1 <= layer.data[k] or layer.data[k] <= 3 then
                 -- Grass/Water
+                -- TODO create floor
                 local x1 = x * tilemap.tilewidth
                 local y1 = y * tilemap.tileheight
                 local x2 = (x + 1) * tilemap.tilewidth
                 local y2 = (y + 1) * tilemap.tileheight
                 colliders:add(x1, y1, x2, y2)
-                -- TODO create floor
             elseif layer.data[k] == 4 then
                 -- Dirt
                 local x1 = x * tilemap.tilewidth
@@ -43,12 +55,27 @@ function Scene:new(path)
     end
 
     local scene = {
-        sheet = sheet,
+        tileset = tileset,
         layer = layer,
         floor = floor,
         colliders = colliders
     }
     setmetatable(scene, self)
+
+    function scene:draw(pos)
+        for x = 0, self.layer.height - 1 do
+            for y = 0, self.width - 1 do
+                local k = y * self.layer.width + x + 1
+                if self.data[k] ~= 0 then
+                    local i = self.data[k] - 1
+
+                    local image_pos = Vec:new(i % self.tileset.columns, math.floor(i / self.columns))
+
+                    local quad = love.graphics.newQuad()
+                end
+            end
+        end
+    end
 
     return scene
 end
