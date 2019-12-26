2c7209c2a52d3b33e2cf37573ba6238647a69f9a
Edws <eduardos.rocha17@gmail.com>
2019-12-26 13:18:54 -0300

    Scene tem o sheet e layer
    scene tem sheet e layer
    colliders ainda não está implementado por completo

--- game/scene.lua
+++ game/scene.lua
@@ -6,10 +6,32 @@ local Scene = {}
 Scene.__index = Scene
 
 function Scene:new(path)
-    local scene = {colliders = Colliders:new()}
-    function scene:load(path)
-        local tilemap = require("tilemap/" .. path)
-        local tileset = tilemap.tilesets[1]
-        local sheet = love.graphics.newImage(tileset.image:sub(3))
+    local tilemap = require("tilemap/" .. path)
+    -- sheet
+    local tileset = tilemap.tilesets[1]
+    local sheet = love.graphics.newImage(tileset.image:sub(3))
+    sheet:setFilter("nearest", "nearest")
+
+    -- layer
+    local layer = tilemap.layers[1]
+
+    -- colliders
+    local floor = Colliders:new()
+    local colliders = Colliders:new()
+    for x = 0, layer.height - 1 do
+        for y = 0, layer.width - 1 do
+            local k = y * self.width + x + 1
+            if layer.data[k] == 0 then --TODO
+            elseif layer.data[k] == 0 then
+            end
+        end
     end
+
+    local scene = {
+        sheet = sheet,
+        layer = layer,
+        colliders = colliders
+    }
+
+    return scene
 end
