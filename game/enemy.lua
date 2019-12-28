e1c91cb442f0b884e6480ba069d01e96974ba82e
Edws <eduardos.rocha17@gmail.com>
2019-12-28 15:27:54 -0300

    Criação da Classe Enemy

---
+++ game/enemy.lua
@@ -0,0 +1,22 @@
+Modules = Modules or require "modules"
+local Vec = Modules.Vec
+
+local Enemy = {}
+Enemy.__index = Enemy
+
+function Enemy:new(pos, vel, acc)
+    local enemy = {
+        pos = pos or Vec:new(),
+        vel = vel or Vec:new(),
+        acc = acc or Vec:new()
+    }
+    setmetatable(enemy, self)
+
+    function enemy:update(dt)
+        self.vel = self.vel + self.acc * dt
+        self.pos = self.vel + self.pos * dt
+    end
+
+    return enemy
+end
+return Enemy
