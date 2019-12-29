3efac77d16c1865afaa65b4c4116e15fa423ebd2
Edws <eduardos.rocha17@gmail.com>
2019-12-29 11:34:18 -0300

    Enemy funcão de virar

--- game/enemy.lua
+++ game/enemy.lua
@@ -5,18 +5,23 @@ local Vec = Modules.Vec
 local Enemy = {}
 Enemy.__index = Enemy
 
-function Enemy:new(pos, vel, acc)
+function Enemy:new(el, pos, vel, acc)
     local enemy = {
         pos = pos or Vec:new(),
         vel = vel or Vec:new(),
         acc = acc or Vec:new(),
-        life = 10
+        life = 10,
+        element = el
     }
     setmetatable(enemy, self)
 
     function enemy:update(dt)
         self.vel = self.vel + self.acc * dt
         self.pos = self.vel + self.pos * dt
+    end
+
+    function enemy:virar()
+        self.vel.x = -self.vel.x
     end
 
     return enemy
